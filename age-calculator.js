7879e54b95bdda9718fd17cf5df1d711ca997e96
janara-ai <jane1998.pls@gmail.com>
2023-04-16 08:05:02 +0400

    committed

---
+++ age-calculator.js
@@ -0,0 +1,14 @@
+//Store the current year in a variable.
+//Store their birth year in a variable.
+//Calculate their 2 possible ages based on the stored values.
+//Output them to the screen like so: "They are either NN or NN", substituting the values.
+
+let currentYear = 2023;
+ let birthYear = 1998;
+ let birthYear2 = 2002;
+
+
+let possibleAge = currentYear - birthYear;
+let possibleAge2 = currentYear - birthYear2;
+
+console.log(' They are either ' + possibleAge + ' or ' + possibleAge2);
